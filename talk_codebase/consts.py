ca7f68184014c0ed4e09d7b5dc75a42133e1bae9
Saryev Rustam <r.saryev@indriver.com>
2023-06-14 14:23:29 +0300

    Update poetry version and consts.py defaults.

--- talk_codebase/consts.py
+++ talk_codebase/consts.py
@@ -12,10 +12,10 @@ MODEL_TYPES = {
     "LOCAL": "local",
 }
 DEFAULT_CONFIG = {
-    "max_tokens": "1048",
-    "chunk_size": "500",
-    "chunk_overlap": "50",
-    "k": "4",
+    "max_tokens": "2056",
+    "chunk_size": "2056",
+    "chunk_overlap": "256",
+    "k": "1",
     "model_name": "gpt-3.5-turbo-0613",
     "model_path": "models/ggml-gpt4all-j-v1.3-groovy.bin",
     "model_type": MODEL_TYPES["OPENAI"],
